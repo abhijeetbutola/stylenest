6a12d0b252abe2b05880a6fe77f10bc75cf29eb5
Abhijeet Butola <abhijeet.butola.com>
2025-01-24 19:30:47 +0530

    cosmetic changes

--- src/components/product-grid/index.tsx
+++ src/components/product-grid/index.tsx
@@ -8,7 +8,7 @@ type ProductGridProps = {
 function ProductGrid({products}: ProductGridProps) {
 
     return (
-        <div className="bg-white pt-24">
+        <div className="bg-white pt-24 max-md:pb-24">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] grid-rows-auto place-items-center gap-8 w-full font-semibold">
                 {products?.map((item) => (
                         <div key={item.product_id}>
